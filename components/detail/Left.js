d3d4896d22a0cdfee2c9a869ca25211837f6522e
adwaith182 <discord223812@gmail.com>
2024-06-16 13:34:21 +0530

    z-10

--- components/detail/Left.js
+++ components/detail/Left.js
@@ -255,7 +255,7 @@ const Left = () => {
                 )}
               />
 
-              <div className="fixed bottom-0 left-0 right-0 bg-white p-4 shadow-md border-t border-gray-200">
+              <div className="fixed bottom-0 left-0 right-0 bg-white z-10 p-4 shadow-md border-t border-gray-200">
                 <div className="flex justify-between items-center">
                   <span className="text-lg font-semibold flex flex-col">
                     <div className="flex items-center">
