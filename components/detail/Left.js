c53c0157d8faf117581db4542026382617881a58
adwaith182 <discord223812@gmail.com>
2024-06-28 21:08:43 +0530

    min decreased

--- components/detail/Left.js
+++ components/detail/Left.js
@@ -452,7 +452,7 @@ const Left = () => {
                         className="rounded-secondary h-10 w-full flex-1"
                         defaultValue={field.value || tour?.members}
                         value={field.value}
-                        min="2"
+                        min="1"
                         onChange={(e) =>
                           field.onChange(parseInt(e.target.value))
                         }
