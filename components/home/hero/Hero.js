fbe51546dcf5abcc984b8b2613f1c5ce4d5a6937
the23villa <the23villa@gmail.com>
2025-01-19 11:33:43 +0530

    Update Hero.js

--- components/home/hero/Hero.js
+++ components/home/hero/Hero.js
@@ -6,12 +6,6 @@ import HeroSearch from "./HeroSearch";
 
 const Hero = () => {
   const [isMobile, setIsMobile] = useState(false);
-  const [currentImage, setCurrentImage] = useState(0);
-
-  const images = [
-    "/assets/home-page/banner/wall3.jpg",
-    "/assets/home-page/banner/wall1.jpg",
-  ];
 
   useEffect(() => {
     const handleResize = () => {
@@ -24,21 +18,13 @@ const Hero = () => {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentImage((prevImage) => (prevImage + 1) % images.length);
-    }, 5000);
-
-    return () => clearInterval(intervalId);
-  }, []);
-
   return (
     <section
-      className="pt-40 sm:pt-40 h-screen bg-no-repeat bg-cover bg-bottom transition-all duration-1000"
-      style={{
-        backgroundImage: `url(${images[currentImage]})`,
-      }}
-    >
+    className="pt-40 sm:pt-40 h-screen bg-no-repeat bg-cover bg-bottom"
+    style={{
+      backgroundImage: "url(/assets/home-page/banner/wall3.jpg)",
+    }}
+  >
       <Container className="h-full">
         <div className="flex flex-col justify-center h-full gap-y-12">
           <div className="grid md:grid-cols-12 md:items-center gap-12 grid-cols-1">
