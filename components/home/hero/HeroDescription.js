e4e8b1e84e92d483c4ab4f3385a792f51de09b1f
the23villa <the23villa@gmail.com>
2025-01-19 11:34:07 +0530

    Update HeroDescription.js

--- components/home/hero/HeroDescription.js
+++ components/home/hero/HeroDescription.js
@@ -1,27 +1,16 @@
 import HighlightText from "@/components/shared/highlightText/HighlightText";
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 const HeroDescription = () => {
-  const [isWhiteText, setIsWhiteText] = useState(true);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setIsWhiteText((prevState) => !prevState);
-    }, 5000);
-
-    return () => clearInterval(intervalId);
-  }, []);
-
-  const textColor = isWhiteText ? "text-white" : "text-black";
 
   return (
     <section className="lg:col-span-8 md:col-span-6 md:px-8 md:py-12 lg:py-1">
       <article className="flex flex-col gap-y-12 items-center md:items-start">
         <div className="flex flex-col gap-y-8 text-center md:text-left">
           <h1
-            className={`text-4xl sm:text-5xl md:text-5xl lg:text-7xl ${textColor} whitespace-normal leading-tight transition-colors duration-1000`}
+            className={`text-4xl sm:text-5xl md:text-5xl lg:text-7xl text-white`}
           >
-            Book Your Dream Stay And Make Memory
+             Book Your Private Nest
           </h1>
         </div>
       </article>
